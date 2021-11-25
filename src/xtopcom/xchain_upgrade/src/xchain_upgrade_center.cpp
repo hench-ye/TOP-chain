70470a22eaf6ee48bc22e5a09ccb9e92eb84de0c
PaytonWu <payton.wu@outlook.com>
2021-11-25 15:20:30 +0800

    feat(fork): add fork point data fro 'archive miner type'

--- src/xtopcom/xchain_upgrade/src/xchain_upgrade_center.cpp
+++ src/xtopcom/xchain_upgrade/src/xchain_upgrade_center.cpp
@@ -18,6 +18,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 0, "block fork point for table receipt"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 0, "enable archive miner type"},
         };
 
         // !!!change!!! fork time for galileo
@@ -25,6 +26,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 0, "block fork point for table receipt"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 0, "enable archive miner type"},
         };
 
         // !!!change!!! fork time for local develop net
@@ -32,12 +34,14 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 0, "block fork point for table receipt"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 0, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 0, "enable archive miner type"},
         };
 #else   // #if defined(XCHAIN_FORKED_BY_DEFAULT)
         xchain_fork_config_t  mainnet_chain_config{
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "block fork point for table receipt"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "enable archive miner type"},
         };
 
         // !!!change!!! fork time for galileo
@@ -45,6 +49,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "block fork point for table receipt"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "enable archive miner type"},
         };
 
         // !!!change!!! fork time for local develop net
@@ -52,6 +57,7 @@ namespace top {
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "block fork point for table receipt"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table statistic info fork point"},
             xfork_point_t{xfork_point_type_t::logic_time, 10000000, "table receipt protocol fork point"},
+            xfork_point_t{xfork_point_type_t::logic_time, 10000000, "enable archive miner type"},
         };
 #endif  // #if defined(XCHAIN_FORKED_BY_DEFAULT)
 
